cd4552368c55c7104125a32c04978e2218cbb5cb
Manil S. Malla <manilsmalla@gmail.com>
2021-04-01 14:30:47 +0545

    Make minor changes.

--- learning_mern/backend/server.js
+++ learning_mern/backend/server.js
@@ -11,7 +11,7 @@ const usersRouter = require('./routes/users');
 
 //  Creating Express Server
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 //  Connect to MongoDB database.
 const URI = process.env.ATLAS_URI;
--- learning_mern/src/App.js
+++ learning_mern/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Navbar from "./components/navbar.component";
-import ExerciseList from "./components/exerciseList.component";
+import ListExercise from "./components/listExercise.component";
 import EditExercise from "./components/editExercise.component";
 import CreateExercise from "./components/createExercise.component";
 import CreateUser from "./components/createUser.component";
@@ -15,7 +15,7 @@ function App() {
      
       <div class= "container">
         <Navbar />
-        <Route path= "/" exact component= {ExerciseList} />
+        <Route path= "/" exact component= {ListExercise} />
         <Route path= "/edit/:id" component= {EditExercise} />
         <Route path= "/create" component= {CreateExercise} />
         <Route path= "/user" component= {CreateUser} />
